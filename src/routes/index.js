a28db9c2949dfc259ade5a16ff804c401a72c7f4
jonaScriptCPU33 <jonascript.cpu@gmail.com>
2022-01-21 09:34:19 -0500

    Se genera ruta Pedidos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Importe de todas las rutas:
-const usersRoute = require('./users.js'); 
-const productDetails = require('./productDetails.js');
-const allProducts = require('./allProducts')
-const postAccount = require('./routePostAccount')
-const categoriesRoute = require('./categories.js');
+const usersRoute = require("./users.js");
+const productDetails = require("./productDetails.js");
+const allProducts = require("./allProducts");
+const postAccount = require("./routePostAccount");
+const categoriesRoute = require("./categories.js");
+const ordersAdmin = require("./ordersAdmin");
 
 /*          Configuracion de rutas:        */
 // USER:
 router.use(usersRoute);
 
 // PRODUCT:
-router.use('/products', allProducts);
-router.use('/productsDetails/id', productDetails);
-router.use('/postAccount',postAccount )
+router.use("/products", allProducts);
+router.use("/productsDetails/id", productDetails);
+router.use("/postAccount", postAccount);
+router.use("/ordersAdmin", ordersAdmin);
 
 //CATEGORIES:
 router.use(categoriesRoute);
-
 
 module.exports = router;
