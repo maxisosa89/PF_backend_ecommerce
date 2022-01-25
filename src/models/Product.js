99e37a6a12dfca415939773e4c7e7e4985aa3b27
ElianZK <elianh2015@gmail.com>
2022-01-25 15:06:36 -0300

    pre pull

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,6 +3,9 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // Defino el modelo:
   sequelize.define("product", {
+    idProduct:{
+      type: DataTypes.UUID
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
