9a1c95044af640b752a574cebfba71e07db522e3
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-26 12:04:08 -0500

    add post and put route to categories

--- src/routes/categories/postCategories.js
+++ src/routes/categories/postCategories.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const { Categories } = require("../../db.js");
 
 const postCategories = async (req, res) => {
-    const { name, img } = req.body;
-    const active = true;
-    let categoryCreate = await Categories.findOrCreate({
-        where: { name },
-        defaults: {
-          name,
-          active,
-          img
-        },
-      });
-    res.send(categoryCreate);
+  const { name, img, active } = req.body;
+  let [categoryCreate, boolean] = await Categories.findOrCreate({
+    where: { name },
+    defaults: {
+      name,
+      active,
+      img,
+    },
+  });
+
+  res.send(categoryCreate);
 };
 
 module.exports = { postCategories };
