6d13e9c5b12fce97bb8185c9258b0b98117b275a
Ignacio <ignaciogrillocaimary@gmail.com>
2022-01-27 19:13:23 -0300

    putprod

--- src/routes/products/getProductDetail.js
+++ src/routes/products/getProductDetail.js
@@ -1,15 +1,27 @@
 const router = require("express").Router();
-const { Product } = require("../../db.js");
+const { Product, Categories } = require("../../db.js");
 
 const getProductDetail = async (req, res) => {
   const { id } = req.params;
+  console.log(id)
   try {
-    const Detail = await Product.findOne({
-      where: {
-        id,
-      },
-    });
-    res.json(Detail);
+    const detail = await Product.findOne(
+      {
+        where: { id },
+        include: { 
+          model: Categories,
+          attributes: ["name"],
+          through: {
+            attributes: []
+          }
+        }
+      }
+    );
+    
+    console.log(detail);
+
+    res.json(detail);
+  
   } catch (error) {
     res.json(error);
   }
