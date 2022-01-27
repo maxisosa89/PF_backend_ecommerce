80f28c150ba88f11e5bf46e9e1c60a49d571e888
ElianZK <elianh2015@gmail.com>
2022-01-27 18:26:54 -0300

    added id to model

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,9 +1,14 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
     sequelize.define('cart', {
+        id:{
+          type:DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: UUIDV4
+        },
         amount : {
           type:DataTypes.INTEGER,
           allowNull: false,
