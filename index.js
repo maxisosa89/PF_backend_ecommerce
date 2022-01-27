875a4b9bef083ff6b6af3d11d935bfbd348626f2
elecalderon1501 <elecalderon.ec@gmail.com>
2022-01-27 15:19:30 -0300

    cambios en index

--- index.js
+++ index.js
@@ -17,17 +17,121 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
+const server = require('./src/app.js')
+const { conn } = require('./src/db.js')
+const { Product, Categories } = require('../PF_backend_ecommerce/src/db')
 
 // let bool = true
 
 // if (bool)
 // bulk
+var images = [
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/19_4-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/16-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/15_4-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/4_4-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/20_3-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/17_4-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/21_4-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/21_4-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/13-370x497.jpg',
+  'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/5-370x497.jpg',
+]
 
+var names = [
+  'Baheera’s Winter Jacket',
+  'Crescent Bay Wallet Grey',
+  'Men’s Shorts',
+  'Men’s Shorts',
+  'Knitted Gloves Turquoise',
+  'White top',
+  'Women’s T-Shirt',
+  'Grey’s Metal Belt',
+  'Mi-Pac Peruvian Stripe Backpack',
+  'bad clothe',
+]
+
+var qty = 30
+let arr = []
+
+while (qty > 0) {
+  qty -= 1
+  var index = images[Math.round(Math.random() * 9)]
+  arr.push({
+    name: names[Math.round(Math.random() * 9)],
+    img: [index, index, index, index, index],
+    ranking: Math.round(Math.random() * 5),
+    price: Math.round(Math.random() * 100) + 50,
+    description:
+      'es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500',
+    aditionalInformation:
+      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
+    stock: {
+      xs: Math.round(Math.random() * 10),
+      s: Math.round(Math.random() * 10),
+      m: Math.round(Math.random() * 10),
+      l: Math.round(Math.random() * 10),
+      xl: Math.round(Math.random() * 10),
+      xxl: Math.round(Math.random() * 10),
+    },
+  })
+}
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-  });
-});
+  server.listen(3001, async () => {
+    console.log('%s listening at 3001') // eslint-disable-line no-
+    const data = [
+      {
+        name: 'Women Clothing',
+        active: true,
+        img: 'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/1-370x497.jpg',
+      },
+      {
+        name: 'Men Clothing',
+        active: true,
+        img: 'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/13-370x497.jpg',
+      },
+      {
+        name: 'Dresses',
+        active: true,
+        img: 'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/18_4-370x497.jpg',
+      },
+      {
+        name: 'Jeans',
+        active: true,
+        img: 'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/4_3-370x497.jpg',
+      },
+      {
+        name: 'Shoes',
+        active: true,
+        img: 'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/2-370x497.jpg',
+      },
+      {
+        name: 'Lingerie',
+        active: true,
+        img: 'https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/14_1-370x497.jpg',
+      },
+    ]
+    cat = await Categories.bulkCreate(data)
+    const allProducts = await Product.findAll()
+    !allProducts.length && (await Product.bulkCreate(arr))
+
+    allProducts.map(async el => {
+      const findedCategory = await Categories.findOne({
+        where: {
+          name: allCategories[
+            Math.round((allCategories.length - 1) * Math.random())
+          ].name,
+        },
+      })
+
+      const findedProduct = await Product.findOne({
+        where: {
+          name: el.name,
+        },
+      })
+
+      findedProduct.addCategories(findedCategory)
+    })
+  })
+})
