818e2bba50b86074d53c0ff82101657c489ddb6b
etchepared <etcheparede@gmail.com>
2022-01-27 20:00:52 -0300

    heroku try 1

--- index.js
+++ index.js
@@ -20,14 +20,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-// let bool = true
-
-// if (bool)
-// bulk
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, async () => {
+    console.log("%s listening at ", port); // eslint-disable-line no-console
   });
 });
