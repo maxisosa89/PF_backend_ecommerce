4bde442bb462164bdd60930415323752d751fc96
Ignacio <ignaciogrillocaimary@gmail.com>
2022-01-28 14:52:29 -0300

    solution additionalInformation

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ while (qty > 0) {
     price: Math.round(Math.random() * 100) + 50,
     description:
       "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
-    aditionalInformation: {
+    additionalInformation: {
       manufacturer: "DataTypes.STRING",
       material: "DataTypes.STRING",
       occasion: "DataTypes.STRING",
