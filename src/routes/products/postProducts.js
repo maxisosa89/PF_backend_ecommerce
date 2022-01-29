52d59527b92a3efa98dc21b1e9ca9a655e71d54e
Ignacio <ignaciogrillocaimary@gmail.com>
2022-01-29 00:12:56 -0300

    post product

--- src/routes/products/postProducts.js
+++ src/routes/products/postProducts.js
@@ -1,7 +1,7 @@
 const { Product, Categories } = require('../../db.js');
 
 
-const postProducts = async (req, res,next) => {
+const updateProducts = async (req, res, next) => {
   
   const { name, img, price, description, aditionalInformation, stock, categories } = req.body;
   
@@ -21,16 +21,16 @@ const postProducts = async (req, res,next) => {
       }
     );
 
-    // categories.map(async (c) => {
-    //   let category = await Categories.findOne(
-    //     {
-    //       where: { name: c }
-    //     }
-    //   )
+    created && categories.map(async (c) => {
+      let category = await Categories.findOne(
+        {
+          where: { name: c }
+        }
+      );
 
-    //   newProduct.addCategory(category)
+      newProduct.addCategory(category)
 
-    // });
+    });
 
 
     res.status(200).json(newProduct);
@@ -39,12 +39,11 @@ const postProducts = async (req, res,next) => {
     next( error );
 
   }
+
 };
 
 
-module.exports = {
-  postProducts,
-};
+module.exports = { updateProducts };
 
 /* JSON prueba para postman: post --> se guarda en la bd.
 
