cad05c5ce8b93c66cdba7a06dca593bac5fe1dfe
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-01-29 19:01:21 -0500

    promos route corrections

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,7 +43,7 @@ router.put("/products/:ProductId", putProducts);
 //PROMOS:
 router.get("/promos", getPromos);
 router.post("/promos", postPromos);
-router.put("/promos/:PromosId", putPromos);
+router.put("/promos", putPromos);
 
 //CATEGORIES:
 router.get("/categories", getCategories);
