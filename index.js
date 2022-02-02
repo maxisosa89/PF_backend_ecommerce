a14ef4b43353eaf9a824e47bf06c0ab059e567e1
etchepared <88214684+etchepared@users.noreply.github.com>
2022-02-02 13:09:14 -0300

    comment bullcreate

--- index.js
+++ index.js
@@ -35,80 +35,80 @@ const {
 
 // if (bool)
 // bulk
-var images = [
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/19_4-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/16-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/15_4-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/4_4-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/20_3-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/17_4-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/21_4-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/21_4-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/13-370x497.jpg",
-  "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/5-370x497.jpg",
-];
+// var images = [
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/19_4-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/16-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/15_4-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/4_4-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/20_3-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/17_4-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/21_4-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/21_4-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/13-370x497.jpg",
+//   "https://ld-wp.template-help.com/woocommerce_59038/wp-content/uploads/2016/06/5-370x497.jpg",
+// ];
 
-var names = [
-  "Baheera’s Winter Jacket",
-  "Crescent Bay Wallet Grey",
-  "Men’s Shorts",
-  "Men’s Shorts",
-  "Knitted Gloves Turquoise",
-  "White top",
-  "Women’s T-Shirt",
-  "Grey’s Metal Belt",
-  "Mi-Pac Peruvian Stripe Backpack",
-  "bad clothe",
-];
+// var names = [
+//   "Baheera’s Winter Jacket",
+//   "Crescent Bay Wallet Grey",
+//   "Men’s Shorts",
+//   "Men’s Shorts",
+//   "Knitted Gloves Turquoise",
+//   "White top",
+//   "Women’s T-Shirt",
+//   "Grey’s Metal Belt",
+//   "Mi-Pac Peruvian Stripe Backpack",
+//   "bad clothe",
+// ];
 
-var qty = 30;
-let arr = [];
+// var qty = 30;
+// let arr = [];
 
-while (qty > 0) {
-  qty -= 1;
-  var index = images[Math.round(Math.random() * 9)];
-  arr.push({
-    name: names[Math.round(Math.random() * 9)],
-    img: [index, index, index, index, index],
-    ranking: Math.round(Math.random() * 5),
-    price: Math.round(Math.random() * 100) + 50,
-    description:
-      "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
-    additionalInformation: {
-      manufacturer: "DataTypes.STRING",
-      material: "DataTypes.STRING",
-      occasion: "DataTypes.STRING",
-      fit: "DataTypes.STRING",
-      lining_material: "DataTypes.STRING",
-    },
-    stock: {
-      xs: Math.round(Math.random() * 10),
-      s: Math.round(Math.random() * 10),
-      m: Math.round(Math.random() * 10),
-      l: Math.round(Math.random() * 10),
-      xl: Math.round(Math.random() * 10),
-      xxl: Math.round(Math.random() * 10),
-    },
-  });
-}
+// while (qty > 0) {
+//   qty -= 1;
+//   var index = images[Math.round(Math.random() * 9)];
+//   arr.push({
+//     name: names[Math.round(Math.random() * 9)],
+//     img: [index, index, index, index, index],
+//     ranking: Math.round(Math.random() * 5),
+//     price: Math.round(Math.random() * 100) + 50,
+//     description:
+//       "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
+//     additionalInformation: {
+//       manufacturer: "DataTypes.STRING",
+//       material: "DataTypes.STRING",
+//       occasion: "DataTypes.STRING",
+//       fit: "DataTypes.STRING",
+//       lining_material: "DataTypes.STRING",
+//     },
+//     stock: {
+//       xs: Math.round(Math.random() * 10),
+//       s: Math.round(Math.random() * 10),
+//       m: Math.round(Math.random() * 10),
+//       l: Math.round(Math.random() * 10),
+//       xl: Math.round(Math.random() * 10),
+//       xxl: Math.round(Math.random() * 10),
+//     },
+//   });
+// }
 
-var defaultPromos = [
-  {
-    title: "Gran promoción!",
-    img: "https://image.freepik.com/foto-gratis/foto-calle-hermosa-chica-anteojos-rojos-capucha-vestido-chaqueta-colorida-verde_8353-7008.jpg",
-    resume: "Por la compra de dos unidades te damos las gracias",
-  },
-  {
-    title: "ültima hora :O!",
-    img: "https://image.freepik.com/foto-gratis/mujer-hispana-riendo-alegremente-pared-graffiti-flores_181624-46424.jpg",
-    resume: "50% de descuento en una prenda tendencia",
-  },
-  {
-    title: "Puedes creer que..!",
-    img: "https://image.freepik.com/foto-gratis/mujer-hermosa-calle_144627-11073.jpg",
-    resume: "Compra 5 unidades y multiplicas el precio unitario por 5",
-  },
-];
+// var defaultPromos = [
+//   {
+//     title: "Gran promoción!",
+//     img: "https://image.freepik.com/foto-gratis/foto-calle-hermosa-chica-anteojos-rojos-capucha-vestido-chaqueta-colorida-verde_8353-7008.jpg",
+//     resume: "Por la compra de dos unidades te damos las gracias",
+//   },
+//   {
+//     title: "ültima hora :O!",
+//     img: "https://image.freepik.com/foto-gratis/mujer-hispana-riendo-alegremente-pared-graffiti-flores_181624-46424.jpg",
+//     resume: "50% de descuento en una prenda tendencia",
+//   },
+//   {
+//     title: "Puedes creer que..!",
+//     img: "https://image.freepik.com/foto-gratis/mujer-hermosa-calle_144627-11073.jpg",
+//     resume: "Compra 5 unidades y multiplicas el precio unitario por 5",
+//   },
+// ];
 
 const port = process.env.PORT || 3001;
 
@@ -128,44 +128,44 @@ conn.sync({ force: true }).then(() => {
     // await Users.sync({force:variable});
     // await Product_categories.sync({force:variable});
 
-    const data = [
-      {
-        name: "Women Clothing",
-        active: true,
-        img: "https://image.freepik.com/foto-gratis/chica-adolescente-alegre-rastas-dientes-dorados-hace-gesto-paz-o-victoria-hace-graffiti-aerosol-vestida-ropa-moda_273609-47516.jpg",
-      },
-      {
-        name: "Men Clothing",
-        active: true,
-        img: "https://image.freepik.com/foto-gratis/vista-posterior-persona-pie-delante-pared-graffiti-botella-spray_23-2147827678.jpg",
-      },
-      {
-        name: "Dresses",
-        active: true,
-        img: "https://image.freepik.com/foto-gratis/mujer-pie-rainbow-village-taichung-taiwan_335224-610.jpg",
-      },
-      {
-        name: "Jeans",
-        active: true,
-        img: "https://image.freepik.com/foto-gratis/mujer-tiro-completo-posando-graffiti_23-2149028824.jpg",
-      },
-      {
-        name: "Shoes",
-        active: true,
-        img: "https://image.freepik.com/foto-gratis/hombre-corriendo-efecto-exposicion-doble-color_53876-102741.jpg",
-      },
-      {
-        name: "Lingerie",
-        active: true,
-        img: "https://image.freepik.com/foto-gratis/closeup-retrato-joven-mujer-inconformista-sexy-hermosa-labios-rojos-gafas-sol_158538-10.jpg",
-      },
-    ];
+    // const data = [
+    //   {
+    //     name: "Women Clothing",
+    //     active: true,
+    //     img: "https://image.freepik.com/foto-gratis/chica-adolescente-alegre-rastas-dientes-dorados-hace-gesto-paz-o-victoria-hace-graffiti-aerosol-vestida-ropa-moda_273609-47516.jpg",
+    //   },
+    //   {
+    //     name: "Men Clothing",
+    //     active: true,
+    //     img: "https://image.freepik.com/foto-gratis/vista-posterior-persona-pie-delante-pared-graffiti-botella-spray_23-2147827678.jpg",
+    //   },
+    //   {
+    //     name: "Dresses",
+    //     active: true,
+    //     img: "https://image.freepik.com/foto-gratis/mujer-pie-rainbow-village-taichung-taiwan_335224-610.jpg",
+    //   },
+    //   {
+    //     name: "Jeans",
+    //     active: true,
+    //     img: "https://image.freepik.com/foto-gratis/mujer-tiro-completo-posando-graffiti_23-2149028824.jpg",
+    //   },
+    //   {
+    //     name: "Shoes",
+    //     active: true,
+    //     img: "https://image.freepik.com/foto-gratis/hombre-corriendo-efecto-exposicion-doble-color_53876-102741.jpg",
+    //   },
+    //   {
+    //     name: "Lingerie",
+    //     active: true,
+    //     img: "https://image.freepik.com/foto-gratis/closeup-retrato-joven-mujer-inconformista-sexy-hermosa-labios-rojos-gafas-sol_158538-10.jpg",
+    //   },
+    // ];
 
-    cat = await Categories.bulkCreate(data);
-    var allProducts = await Product.findAll();
-    !allProducts.length && (await Product.bulkCreate(arr));
-    allProducts = await Product.findAll();
-    var allCategories = await Categories.findAll();
+    // cat = await Categories.bulkCreate(data);
+    // var allProducts = await Product.findAll();
+    // !allProducts.length && (await Product.bulkCreate(arr));
+    // allProducts = await Product.findAll();
+    // var allCategories = await Categories.findAll();
 
     // allProducts.map(async (el) => {
     //   const findedCategory = await Categories.findOne({
@@ -185,86 +185,86 @@ conn.sync({ force: true }).then(() => {
     //   findedProduct.addCategories(findedCategory);
     // });
 
-    await Promos.bulkCreate(defaultPromos);
+    // await Promos.bulkCreate(defaultPromos);
 
-    allProducts.map(async (p) => {
-      const oneCategory = await Categories.findOne({
-        where: {
-          name: data[Math.round((data.length - 1) * Math.random())].name,
-        },
-      });
-      p.addCategories(oneCategory);
-    });
+    // allProducts.map(async (p) => {
+    //   const oneCategory = await Categories.findOne({
+    //     where: {
+    //       name: data[Math.round((data.length - 1) * Math.random())].name,
+    //     },
+    //   });
+    //   p.addCategories(oneCategory);
+    // });
 
-    var defaultUsers = [
-      {
-        email: "maxisosa89@gmail.com",
-        name: "Maxi",
-        admin: true,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
-      },
+    // var defaultUsers = [
+    //   {
+    //     email: "maxisosa89@gmail.com",
+    //     name: "Maxi",
+    //     admin: true,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
+    //   },
 
-      {
-        email: "elecalderon.ec@gmail.com",
-        name: "Eleo",
-        admin: false,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
-      },
+    //   {
+    //     email: "elecalderon.ec@gmail.com",
+    //     name: "Eleo",
+    //     admin: false,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
+    //   },
 
-      {
-        email: "elianh2015@gmail.com",
-        name: "Elian",
-        admin: false,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
-      },
+    //   {
+    //     email: "elianh2015@gmail.com",
+    //     name: "Elian",
+    //     admin: false,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
+    //   },
 
-      {
-        email: "oiaraque@hotmail.com",
-        name: "Oscar",
-        admin: false,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
-      },
+    //   {
+    //     email: "oiaraque@hotmail.com",
+    //     name: "Oscar",
+    //     admin: false,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
+    //   },
 
-      {
-        email: "jonascript.cpu@gmail.com",
-        name: "Jona",
-        admin: false,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
-      },
+    //   {
+    //     email: "jonascript.cpu@gmail.com",
+    //     name: "Jona",
+    //     admin: false,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
+    //   },
 
-      {
-        email: "ignaciogrillocaimary@gmail.com",
-        name: "Nacho",
-        admin: true,
-        picture:
-          "https://lh3.googleusercontent.com/ogw/ADea4I4DJ5xScae4_3XX-QBkf20nUwbNcD34h4rbxdWd-g=s83-c-mo",
-      },
+    //   {
+    //     email: "ignaciogrillocaimary@gmail.com",
+    //     name: "Nacho",
+    //     admin: true,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/ogw/ADea4I4DJ5xScae4_3XX-QBkf20nUwbNcD34h4rbxdWd-g=s83-c-mo",
+    //   },
 
-      {
-        email: "etcheparede@gmail.com",
-        name: "David",
-        admin: false,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
-      },
+    //   {
+    //     email: "etcheparede@gmail.com",
+    //     name: "David",
+    //     admin: false,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14GhqL0lyCMwVFpA03QxqhzxmMp1fIDXSX7beyb64=s70-p-k-rw-no",
+    //   },
 
-      {
-        email: "nicolasmayorquinduran@gmail.com",
-        name: "Nicolas",
-        admin: true,
-        picture:
-          "https://lh3.googleusercontent.com/a-/AOh14Gj1-uJyQS_inNu3IEJZvImBNJNsLY2uW0-ocrm4=s96-c",
-      },
-    ];
+    //   {
+    //     email: "nicolasmayorquinduran@gmail.com",
+    //     name: "Nicolas",
+    //     admin: true,
+    //     picture:
+    //       "https://lh3.googleusercontent.com/a-/AOh14Gj1-uJyQS_inNu3IEJZvImBNJNsLY2uW0-ocrm4=s96-c",
+    //   },
+    // ];
 
-    let validate = await Users.findAll();
-    if (validate.length === 0) {
-      await Users.bulkCreate(defaultUsers);
-    }
+    // let validate = await Users.findAll();
+    // if (validate.length === 0) {
+    //   await Users.bulkCreate(defaultUsers);
+    // }
   });
 });
