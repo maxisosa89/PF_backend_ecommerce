b21bd1a8afc37c4004406eae3c05d6f7fd777705
Ignacio <ignaciogrillocaimary@gmail.com>
2022-02-03 23:14:00 -0300

    PostProduct finish

--- src/routes/categories/putCategories.js
+++ src/routes/categories/putCategories.js
@@ -1,28 +1,29 @@
-const router = require("express").Router();
 const { Categories } = require("../../db.js");
 
+
 const putCategories = async (req, res,next) => {
+ 
+  const { name, img, active } = req.body;
+  
   try {
-    const { name, img, active } = req.body;
-    const{CategoriesId}=req.params;
-      const info = {
-        img: img,
-        name: name,
-        active: active,
+    const { CategoriesId } = req.params;
+      
+    const info = {
+      img: img,
+      name: name,
+      active: active,
     };
   
     const catById = await Categories.findOne({where:{CategoriesId}});
   
-    // productById
-    // ? await productById.update({ categories: categories })
-    // : console.log("No se ha podido relacionar el producto con la categoria");
-  
     catById
     ? res.send(await catById.update(info))
     : res.send("No se ha podido actualizar la categoria");
+  
   } catch (error) {
     next(error);
   }
+
 };
 
 module.exports = { putCategories };
