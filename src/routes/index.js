ac839f74075e3b1c6850f90b34c27b0bacd39feb
ElianZK <elianh2015@gmail.com>
2022-02-04 22:52:35 -0300

    ya recibe las unidades por talla

--- src/routes/index.js
+++ src/routes/index.js
@@ -42,7 +42,7 @@ router.put("/users", putUser);
 router.get("/carts", getAllCarts)
 router.get("/cart/:email", getUserCart);
 router.put("/cart/:CartId", putUserCart); 
-router.delete("/cart/:CartId", deleteUserCart);
+router.delete("/cart/:CartId/:ProductId", deleteUserCart);
 
 // PRODUCT:
 router.get("/products", getProducts);
