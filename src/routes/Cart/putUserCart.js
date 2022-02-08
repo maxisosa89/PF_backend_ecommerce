86bc32e4b06f7ea86b50d8f9ce6536015b0c3ace
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-02-08 17:59:15 -0500

    solve bug put user cart

--- src/routes/Cart/putUserCart.js
+++ src/routes/Cart/putUserCart.js
@@ -1,52 +1,50 @@
-const {Cart, Users, Product} = require("../../db");
-
+const { Cart, Users, Product } = require("../../db");
 
 const putUserCart = async (req, res, next) => {
   try {
-    const {CartId} = req.params;
-    const {ProductInfo,Status} = req.body
-    // console.log( ProductInfo)
+    const { CartId } = req.params;
+    const ProductInfo = req.body;
     // console.log( ProductInfo[0])
-    
-    if (ProductInfo && !Status) {
-      let ProductId = ProductInfo.ProductId
-      let cart = await Cart.findOne({
-        where: {
-          CartId
-        }
-      });
-      let carrito = cart.productCart
 
-      if (carrito.find(p => p.ProductId === ProductId)) {
-        return res.status(400).json("ESTE PRODUCTO YA EXISTE EN EL CARRITO")
-      }
+    let ProductId = ProductInfo?.ProductId;
+    let cart = await Cart.findOne({
+      where: {
+        CartId,
+      },
+    });
 
-      let product = await Product.findOne({
-        where: {
-          ProductId
-        }
-      })
+    let carrito = cart.productCart;
 
-      infoUpdateCart = {
-        productCart: [...cart.productCart, ProductInfo]
-      }
-      return res.send(await cart.update(infoUpdateCart));
-    } else if (Status && !ProductInfo) {
-      let cart = await Cart.findOne({
-        where: { 
-          CartId
-        }
-      });
-      updatedStatus = {
-        status: Status
-      }
-      return res.status(200).send(await cart.update(updatedStatus))
+    if (carrito.find((p) => p.ProductId === ProductId)) {
+      return res.status(400).json("ESTE PRODUCTO YA EXISTE EN EL CARRITO");
     }
+
+    let product = await Product.findOne({
+      where: {
+        ProductId,
+      },
+    });
+
+    infoUpdateCart = {
+      productCart: [...cart.productCart, ProductInfo],
+    };
+    return res.send(await cart.update(infoUpdateCart));
+    // }
+    //  else if (Status && !ProductInfo) {
+    //   let cart = await Cart.findOne({
+    //     where: {
+    //       CartId,
+    //     },
+    //   });
+    //   updatedStatus = {
+    //     status: Status,
+    //   };
+    //   return res.status(200).send(await cart.update(updatedStatus));
+    // }
   } catch (err) {
     // console.log("Get users/cart/:id", err);
-    next(err)
+    next(err);
   }
 };
 
-module.exports = {putUserCart};
-
+module.exports = { putUserCart };
