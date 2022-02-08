acfa059a70d26df2e085033655c55d6107836dfd
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-02-08 16:18:39 -0500

    change put user cart from params to query

--- src/routes/Cart/getUserCart.js
+++ src/routes/Cart/getUserCart.js
@@ -4,22 +4,11 @@ const getUserCart = async (req, res, next) => {
   try {
     const { email } = req.params;
     //[Busco el usuario
-<<<<<<< HEAD
-    let cart = await Cart.findOne({
+    let cart = await Cart.findAll({
       where: {
         user: email,
-        status: "open",
       },
     });
-
-    console.log(email);
-=======
-    let cart = await Cart.findAll({
-      where:{
-        user:email
-      }
-    })
->>>>>>> a92ffd4a26685d79b7eff18ae7adcbb72ed46140
 
     return res.send(cart);
   } catch (err) {
