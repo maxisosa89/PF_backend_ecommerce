5b277d899e511d57709020247a63142c8016f88e
nicolasmayorquin <nicolasmayorquinduran@gmail.com>
2022-02-07 19:13:11 -0500

    no apply

--- src/routes/Cart/getUserCart.js
+++ src/routes/Cart/getUserCart.js
@@ -1,22 +1,23 @@
-const {Cart, Users} = require("../../db");
+const { Cart, Users } = require("../../db");
 
-
-const getUserCart = async (req,res,next)=>{
-  try{
-    const {email} = req.params;
+const getUserCart = async (req, res, next) => {
+  try {
+    const { email } = req.params;
     //[Busco el usuario
     let cart = await Cart.findOne({
-      where:{
-        user:email,
-        status: "open"
-      }
-    })
+      where: {
+        user: email,
+        status: "open",
+      },
+    });
+
+    console.log(email);
 
     return res.send(cart);
-  }catch(err){
+  } catch (err) {
     console.log("Get users/cart/:Userid", err);
-    next(err)
+    next(err);
   }
-}; 
+};
 
-module.exports = {getUserCart};
+module.exports = { getUserCart };
